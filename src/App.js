6df7c07a53cd107b8032c7b4af459fddc06fcc23
GavireddyMadhav <gavireddymadhavreddy@gmail.com>
2022-01-22 13:00:58 +0530

    Four

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Fet from './component/Fet';
+
+function App() {
+  return (
+    <div className="App">
+    <Fet/>
+    </div>
+  );
+}
+
+export default App;
